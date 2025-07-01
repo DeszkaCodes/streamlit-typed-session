07e1490cbe59c1dbd27d69923298ad6720f65c37
Ormándi Norbert <60075235+DeszkaCodes@users.noreply.github.com>
2025-07-01 23:58:13 +0200

    feat: add mytypes module

---
+++ src/streamlit_typed_session/mytypes.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from collections.abc import Callable, MutableMapping
+from typing import Any, TypeVar, final
+from streamlit.elements.lib.utils import Key
+
+
+__all__ = [
+    "Unset",
+    "StateVar",
+    "SessionStateKey",
+    "SessionStateValue",
+    "SessionStateLike",
+    "SessionStateProvider",
+]
+
+_T = TypeVar("_T")
+
+
+@final
+class Unset:
+    """Used to mark session properties that are not set."""
+
+
+StateVar = _T | Unset
+
+SessionStateKey = Key
+SessionStateValue = Any
+SessionStateLike = MutableMapping[SessionStateKey, SessionStateValue]
+"""Type that can be used as a session state."""
+
+SessionStateProvider = Callable[[], SessionStateLike]
+"""A callable that takes no arguments and returns a SessionStateLike."""
