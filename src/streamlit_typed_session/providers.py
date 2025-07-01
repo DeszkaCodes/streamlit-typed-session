a7fc5ce880c06c35288b822109bf377020cfa84a
Ormándi Norbert <60075235+DeszkaCodes@users.noreply.github.com>
2025-07-01 23:58:38 +0200

    feat: add providers module

---
+++ src/streamlit_typed_session/providers.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+from streamlit_typed_session.mytypes import SessionStateLike
+
+__all__ = ["streamlit_session_provider"]
+
+
+def streamlit_session_provider() -> SessionStateLike:
+    """The default provider that returns Streamlit's session state.
+
+    Returns:
+        SessionStateLike: The sesion state of Streamlit.
+    """
+    import streamlit
+
+    return streamlit.session_state
